e784d453b95fdeb150c6a593d3648982c0f5ddcd
dnainainai <dnainainai@gmail.com>
2020-12-18 00:37:34 +0900

    Modify getUserMedia

--- src/client.js
+++ src/client.js
@@ -1,19 +1,4 @@
 async function getVideoStreamAndConnect() {
-    navigator.mediaDevices = navigator.mediaDevices || ((navigator.mozGetUserMedia || navigator.webkitGetUserMedia) ? {
-        getUserMedia: function(c) {
-          return new Promise(function(y, n) {
-            (navigator.mozGetUserMedia ||
-             navigator.webkitGetUserMedia).call(navigator, c, y, n);
-          });
-        }
-     } : null);
-     
-     if (!navigator.mediaDevices) {
-       console.log("getUserMedia() not supported.");
-       return;
-     }
-     
-    
      var constraints = { audio: true, video: { width: 1280, height: 720 } };
      navigator.mediaDevices.getUserMedia(constraints)
      .then(function(stream) {
